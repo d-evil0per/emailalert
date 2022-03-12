d23be4250fc8ece210564f0f390d9badbc982c5e
D-eviloper <dhir.xcess@gmail.com>
2022-03-12 14:20:31 +0530

    updated code

--- emailalert.py
+++ emailalert.py
@@ -1,14 +1,9 @@
+from imap_tools import MailBox,AND
 import imaplib
-import email
-from gtts import gTTS
-import os
 import getpass
-import datetime
 import json
-
-
-
-
+from gtts import gTTS
+import playsound
 
 def banner():
     os.system("clear")
@@ -21,70 +16,42 @@ def banner():
     print('╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝   ╚═╝   ')
     print("\n")
 
-
-# #================================ Speak function =================================================
-
 def speak(text):
-    tts = gTTS(text, lang='en')
-    tts.save("output.mp3")
-    os.system("mpg321 output.mp3 --stereo 2>/dev/null;")
+    tts = gTTS(text, lang='en') #gtts API to convert text to speech
+    tts.save("output.mp3") #saving as the audio file
+    playsound.playsound('output.mp3') #playing from audio file
 
-# #========================== Speak function ends =================================================
 
-def read_email_from_gmail(username,currentDT,configdata):
+def read_email_from_email(username,configdata):
     ORG_EMAIL   = configdata['mail']['ORG_EMAIL']
     FROM_EMAIL  = configdata['mail']['FROM_EMAIL'] + ORG_EMAIL
     FROM_PWD    = configdata['mail']['FROM_PWD']
     SMTP_SERVER = configdata['mail']['SMTP_SERVER']
-    SMTP_PORT   = configdata['mail']['SMTP_PORT']
-    mail = imaplib.IMAP4_SSL(SMTP_SERVER)
-    print("Attempting Login into your email account.")
-    mail.login(FROM_EMAIL,FROM_PWD)
-    print("Loggedin!!.")
-    print("Checking inbox!!")
-    mail.select('inbox')
-    print("Checking UNSEEN mails!!")
-    typ,data= mail.search(None, '(UNSEEN)')
-    count = len(data[0].split())
-    # print(count)
-    from_email=[]
-    i=0 
-    if count>0:
-        for eachid in data[0].split():
-            print("id"+str(eachid))
-            if str(eachid)!="" :
-                i+=1
-                uid=eachid
-                print("Fetching Mail details!!")
-                status,message = mail.fetch(uid, '(RFC822)' )
-                for response_part in message:
-                    if isinstance(response_part, tuple):
-                        msg=email.message_from_bytes(response_part[1])
-                        email_from = msg['from'].split(" ")
-                        from_email.append(email_from[0])
-                print("Marking Mail as SEEN!!")
-                success = mail.store(uid, '+FLAGS', '\Seen') 
-            
-        if i>0:
-            if i==1:
-                text=username+", You have an Email From "+from_email[0]+",with a Subject Saying "+msg['subject']
-            else :
-                set_from=set(from_email)
-                from_list=", ".join(set_from)
-                text=username+", You have "+str(i)+" Emails From "+from_list
-                print(text)
-                speak(text)
-    else:
-        print("You Dont have any new emails!!")
-        speak("You Dont have any new emails")
+    mail = MailBox(SMTP_SERVER).login(FROM_EMAIL, FROM_PWD)
+    messages = mail.fetch(criteria=AND(seen=False),mark_seen=True,bulk=True)
 
-banner()
-username = getpass.getuser()
-username=username.replace("-","")
-currentDT = datetime.datetime.now()
-# configuration reading
-print("Loading Configuration...")
-with open('mailconfig.json') as json_file:
-    configdata = json.load(json_file)
-print("Configuration Loaded Successfully...")
-read_email_from_gmail(username,currentDT,configdata)
+    msg=list(messages)
+    count=len(msg)
+    if count>0:
+        text=username+", You have an Email From "+msg[0].from_+",with a Subject Saying "+msg[0].subject
+        print(text)
+        speak(text)
+
+    else:
+        print("You Don't have any new emails!!")
+        speak("You Don't have any new emails")
+
+
+
+if __name__ == "__main__":
+    banner()
+    username = getpass.getuser()
+    username=username.replace("-","")
+    # configuration reading
+    print("Loading Configuration...")
+    with open(r'D:\Codes\Python\emailalert\mailconfig.json') as json_file:
+        configdata = json.load(json_file)
+    print("Configuration Loaded Successfully...")
+    read_email_from_email(username,configdata)
+
+
